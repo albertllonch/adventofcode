8704171d5a4582ed4a4c193560cb3c520b19855b
Albert Llonch <albertllonch@gmail.com>
2022-12-07 23:32:21 +0100

    Day 2 part 2 OK

--- 2022/Day2/Day2.py
+++ 2022/Day2/Day2.py
@@ -1,15 +1,19 @@
 # Day 2
 import numpy as np
 
-input_dict = {"X":0,"Y":1,"Z":2,"A":0,"B":1,"C":2}
-scores = np.matrix('4 8 3; 1 5 9; 7 2 6')
-totalScore = 0
-print(scores)
-file = open("input.txt","r")
-for line in file:
-    a,b = line.strip().split(" ")
-    totalScore = totalScore + scores[input_dict[a],input_dict[b]]
+def getTotalScore(scores):
+    input_dict = {"X":0,"Y":1,"Z":2,"A":0,"B":1,"C":2}
+    totalScore = 0
+    file = open("input.txt","r")
+    for line in file:
+        a,b = line.strip().split(" ")
+        totalScore = totalScore + scores[input_dict[a],input_dict[b]]
+    file.close()
+    return totalScore
 
-print(totalScore)
-    
+scores = np.matrix('4 8 3; 1 5 9; 7 2 6')
+print(getTotalScore(scores))
+scores2 = np.matrix('3 4 8; 1 5 9; 2 6 7')
+print(getTotalScore(scores2))
+
 
